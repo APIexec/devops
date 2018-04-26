4d268fc56c116958709955ec6ef4e6ae90526f10
Tomasz Sapletta <t.sapletta@buchbinder.de>
2018-04-26 15:51:28 +0200

    create bot for wizard, tutorials

---
+++ bot.bat
@@ -0,0 +1,12 @@
+:: Solution for chat with user, can help with asking about some functionality and help with tutorials
+:: EXAMPLE
+:: bot.bat How to Install Wordpress?
+:: Where?
+::  > local/remote/docker
+:: Which version of Wordpress?
+::  > 4.6, 4.7
+:: Which domain ?
+:: Auth Data:
+::  > Generate and put auth to auth folder? / create manully now / get from predefined file?
+::
+
